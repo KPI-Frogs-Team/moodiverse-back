b744ff6f45207e2f065b60271b2cb440d8f8c595
Denys Malanichev <deniskirillov228@gmail.com>
2023-07-02 15:13:46 +0300

    bugfix: Base Domain edited to automatically add CreatedAt and UpdatedAt

--- Moodiverse/Moodiverse.WebAPI/Models/Common/BaseDomain.cs
+++ Moodiverse/Moodiverse.WebAPI/Models/Common/BaseDomain.cs
@@ -3,6 +3,17 @@ namespace Moodiverse.WebAPI.Models.Common;
 public abstract class BaseDomain
 {
     public Guid Id { get; set; }
-    public DateTime CreatedAt { get; set; }
+    private DateTime _createdAt;
+
+    public BaseDomain()
+    {
+        CreatedAt = UpdatedAt = DateTime.Now;
+    }
+
+    public DateTime CreatedAt
+    {
+        get => _createdAt;
+        set => _createdAt = (value == DateTime.MinValue) ? DateTime.Now : value;
+    }
     public DateTime UpdatedAt { get; set; }
 }
