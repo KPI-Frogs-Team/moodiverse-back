82d880458b0500055e0696152c3d5a15ca1a9360
DenysMalanichev <58270142+DenysMalanichev@users.noreply.github.com>
2023-07-06 18:17:01 +0300

    feature/create-Record-domain-and-DTOs (#32)
    feature: Record domain and DTOs added

---
+++ Moodiverse/Moodiverse.WebAPI/Models/DTO/RecordDTO/GetRecordDto.cs
@@ -0,0 +1,8 @@
+namespace Moodiverse.WebAPI.Models.DTO.RecordDTO;
+
+public class GetRecordDto
+{
+    public string Emoji { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+}
---
+++ Moodiverse/Moodiverse.WebAPI/Models/DTO/RecordDTO/RecordRoundStatisticsResponseDto.cs
@@ -0,0 +1,7 @@
+namespace Moodiverse.WebAPI.Models.DTO.RecordDTO;
+
+public class RecordRoundStatisticsResponseDto
+{
+    public string Image { get; set; }
+    public int Count { get; set; }
+}
