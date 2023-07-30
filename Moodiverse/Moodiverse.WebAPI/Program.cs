dea45e11b6d3598fb1707683ba3195ec74b8bae8
DenysMalanichev <58270142+DenysMalanichev@users.noreply.github.com>
2023-07-30 19:11:31 +0300

    feature/add-Automapper-services (#36)
    feature: added AutoMapper service

--- Moodiverse/Moodiverse.WebAPI/Program.cs
+++ Moodiverse/Moodiverse.WebAPI/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Moodiverse.WebAPI.Data;
+using Moodiverse.WebAPI.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("MoodiverseConnectionString");
 builder.Services.AddDbContext<MoodiverseDbContext>(options => { options.UseSqlServer(connectionString); });
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
